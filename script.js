c1965685d1fa05ab3ec668843fb127be0d7a0ed4
Ramiro-Zarate <ramazara1313@gmail.com>
2026-01-05 20:16:15 -0300

    Navbar y sección servicios responsive

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 const seccionNosotros = document.querySelector('#nosotros');
 const textoNosotros = document.querySelector('.nosotros_texto');
+
+const menuHamburguer = document.querySelector('#btn_hamburguesa');
+const menu = document.getElementById("menuLista");
+
+menuHamburguer.addEventListener('click', () => {
+    // Si tiene la clase 'activo' la saca, si no la tiene la agrega
+    menu.classList.toggle("activo");
+});
